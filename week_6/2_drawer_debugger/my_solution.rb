e93af032412e7fdd4984f2db6c18658cfc6dfc04
lrakhman <lana.rakhman@gmail.com>
2014-04-16 14:09:21 -0500

    added #2

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,63 +1,77 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
 
 # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+  def initialize
+	@contents = []
+	@open = true
+  end
+
+  def open
+	@open = true
+  end
+
+  def close
+	@open = false
+  end 
+
+  def add_item(item)
+	@contents << item
+  end
+
+  def remove_item(item = @contents.pop) #what is `#pop` doing?
+	@contents.delete(item)
+  end
+
+  def dump  # what should this method return?
+	@contents.pop(@contents.length)
+	puts "Your drawer is empty."
+	return @contents
+  end
+
+  def view_contents
+  	if @contents.length > 0
+	  puts "The drawer contains:"
+	  @contents.each {|silverware| puts "- " + silverware.type }
+	else
+	  return "Your drawer is empty."
+	end
+  end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+
+  attr_reader :type
 
 # Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+  def initialize(type, clean = true)
+	@type = type
+	@clean = clean
+  end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
+  def eat
+  	@clean = false
+	puts "I'm eating with the #{type}"
+  end
 
+  def clean_silverware
+  	@clean = true
+  end
+
+  def clean
+  	@clean
+  end
 end
 
 knife1 = Silverware.new("knife")
@@ -72,7 +86,6 @@ silverware_drawer.remove_item
 silverware_drawer.view_contents
 sharp_knife = Silverware.new("sharp_knife")
 
-
 silverware_drawer.add_item(sharp_knife)
 
 silverware_drawer.view_contents
@@ -85,18 +98,43 @@ silverware_drawer.view_contents
 silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
 
+fork = Silverware.new("fork")
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+silverware_drawer.add_item(fork)
+silverware_drawer.view_contents
+
+silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+silverware_drawer.view_contents
 fork.eat
+fork.clean_silverware
+
+large_spoon = Silverware.new("large_spoon")
+silverware_drawer.add_item(large_spoon)
+silverware_drawer.view_contents
 
 #BONUS SECTION
-# puts fork.clean
+p fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+def assert
+	yield
+end
 
+p assert { fork.clean == true }
+p assert { fork.eat != "I'm eating with the fork" }
+p assert { fork.eat == nil}
+p assert { silverware_drawer.open == true }
+p assert { silverware_drawer.close == false }
 
+# # 5. Reflection 
 
-
-
-# 5. Reflection 
+# This was super confusing at first, and I'm not really sure why! I think I was just very 
+# tired when I started this challenge. I added some methods and things, but then when
+# I wanted to print out the contents it wouldn't add my "big-spoon". So I left it alone
+# after struggling for a while (which I just learned to do, and is a great way for me to be
+# more productive). Then I came back and realized that yes, it DID work, and that I was
+# just writing it wrong. Good lord, the kinds of dumb mistakes that I make when I'm 
+# staring at the same piece of code for too long is hilarous. Lastly, I'm not absolutely
+# sure why fork.eat produces the value nil, but I'm looking into it now!
+	
